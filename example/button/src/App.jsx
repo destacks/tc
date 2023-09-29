13531be2c31eeba93b08262b1f3414b97c3726d0
Alexis Michaltsis <a.michaltsis@gmail.com>
2023-09-29 21:40:07 +0200

    Fix wording in button example

--- example/button/src/App.jsx
+++ example/button/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-2xl font-bold mb-4">
-          <code className="text-blue-600">tc</code> Button Example with React +
+          <span className="text-blue-600">TC</span> Button Example With React +
           Vite
         </h1>
         <div className="space-y-4">
@@ -30,7 +30,7 @@ function App() {
           </Button>
           <p className="border p-4 rounded bg-gray-100">
             Edit <code className="bg-gray-200 rounded p-1">src/Button.jsx</code>
-            to test <span className="text-blue-600 font-bold">tc</span>
+            to test <span className="text-blue-600 font-bold">TC</span>
           </p>
         </div>
       </div>
